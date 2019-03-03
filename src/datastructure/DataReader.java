1139ad5b44ae196a32bcbbdc7571027ce6792c91
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-02 23:46:43 -0500

    new commit

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,5 +1,11 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -18,8 +24,51 @@ public class DataReader {
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car";
 
+         FileReader fileReader = null;
+		 BufferedReader bufferedReader = null;
+
+		try {
+			fileReader = new FileReader(textFile);
+            bufferedReader = new BufferedReader(fileReader);
+		}catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+			System.out.println("check your path");
+		}
+
+		String st ="";
+		List<String> linkedList = new LinkedList<String>();
+		Stack<String> dataStack = new Stack();
+		try{
+			while((st = bufferedReader.readLine())!= null){
+				linkedList.add(st);
+				dataStack.add(st);
+				System.out.println(st);
+			}
+
+		}catch (IOException ex){
+			ex.printStackTrace();
+			System.out.println("please check your path");
+		}finally {
+			if(fileReader!=null){
+				fileReader =null;
+			}
+			if(bufferedReader != null){
+				bufferedReader =null;
+			}
+		}
+
+		System.out.println(dataStack.peek());
+		System.out.println(dataStack.pop());
+		System.out.println(dataStack.size());
+		for(String data:linkedList){
+			System.out.println(data);
+		}
+		Iterator iter = dataStack.listIterator();
+		while(iter.hasNext()){
+			System.out.println(iter.next());
+		}
 
 
 	}
