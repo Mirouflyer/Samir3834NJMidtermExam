b3793110fcc0a11cd6771515cfc74b1b552a3a9d
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-05 17:01:07 -0500

    new commit

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class FindLowestDifference {
     public static void main(String[] args) {
         /*
@@ -10,11 +12,44 @@ public class FindLowestDifference {
         int [] array1 = {30,12,5,9,2,20,33,1};
         int [] array2 = {18,25,41,47,17,36,14,19};
 
+        int a =array1.length;
+        int b = array2.length;
 
-
+        System.out.println(findSmallestDifference(array1,array2,a,b));
 
 
 
     }
 
+
+   public  static int findSmallestDifference(int A[], int B[],
+                                      int m, int n)
+    {
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int a = 0, b = 0;
+
+
+        int result = Integer.MAX_VALUE;
+
+
+        while (a < m && b < n)
+        {
+            if (Math.abs(A[a] - B[b]) < result)
+                result = Math.abs(A[a] - B[b]);
+
+
+            if (A[a] < B[b])
+                a++;
+
+            else
+                b++;
+        }
+
+        System.out.println(result);
+        return result;
+
+    }
 }
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -18,7 +18,13 @@ public class UnitTestingMath {
         int fib = fibonacci.fibonacci(0);
         Assert.assertEquals(560,560);
 
+        int [] array1 = {30,12,5,9,2,20,33,1};
+        int [] array2 = {18,25,41,47,17,36,14,19};
 
+        int a =array1.length;
+        int b = array2.length;
+        int small =FindLowestDifference.findSmallestDifference(array1,array2,a,b);
+        Assert.assertEquals(1,1);
 
     }
 }
